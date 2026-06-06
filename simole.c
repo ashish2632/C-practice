a7e19457d7b79db486a5e0a48e61ba45db4f9635
ashish2632 <ashish2632a@gmail.com>
2026-06-06 16:40:48 +0530

    Day2: arrays basics - pehla dusra jod

---
+++ simole.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main() {
+ int num[2];
+
+num[0]=5;
+num[1]=7;
+printf("pehla = %d\n", num[0]);
+printf("dusra = %d\n", num[1]);
+printf("jod = %d\n", num[0] + num[1]);
+return 0;
+}

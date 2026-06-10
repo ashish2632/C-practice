657acc7ca9dc7ad218e5e707b4dc2078cf0ffef6
ashish2632 <ashish2632a@gmail.com>
2026-06-10 19:26:11 +0530

    add equal check program

---
+++ equal.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+ int main() {
+int a,b;
+printf("Enter a=");
+scanf("%d", &a);
+ printf("Enter b=");
+scanf("%d", &b);
+ 
+   if(a==b)
+{
+printf("\na and b are equal.");
+}
+else
+{
+  printf("\na and b are not equal.");
+  }
+return 0;
+}

e951775ac476d965e1a4f98a219c500887f730f7
Ashish2632 <ashish2632a@gmail.com>
2026-06-14 02:02:11 +0000

    average of 4 numbers

--- average.c
+++ average.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-int a,b,c;
+int a,b,c,d;
 float avg;
-printf("Enter 3 numbers: ");
-scanf("%d%d%d",&a,&b,&c);
-avg=(a+b+c)/3.0;
+printf("Enter 4 numbers: ");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+avg=(a+b+c+d)/4.0;
 printf("Average=%.2f\n",avg);
 return 0;
 }

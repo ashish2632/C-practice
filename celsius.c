b82ee0068f87faa5e5ba5a5f8cac73bb1a3ea8a5
Ashish2632 <ashish2632a@gmail.com>
2026-06-17 13:05:37 +0000

    fahrenheit to celsius convert

---
+++ celsius.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float f, c;
+    
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &f);
+    
+    c = (f - 32) * 5 / 9;
+    
+    printf("%.2f°F = %.2f°C\n", f, c);
+    
+    return 0;
+}

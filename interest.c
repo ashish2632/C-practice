e9b7edd244bff25b6c3a96dfce85e6e019566f8c
Ashish2632 <ashish2632a@gmail.com>
2026-06-18 06:21:36 +0000

    simple interest

---
+++ interest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+ float p, r, t;
+printf("Enter the princile amount:");
+scanf("%f", &p);
+printf("Enter the ratio of interest:");
+scanf("%f", &r);
+printf("Enter the time:");
+scanf("%f", &t);
+printf("the simple interest is %f",(p*r*t)/100);
+return 0;
+}
+
--- simple.c
+++ simple.c
@@ -1,7 +1,9 @@
 #include  <stdio.h>  
   int main(){
-int r = 5;
-  printf("the area of circle with radius %d is %f", r, 3.14*r*r);
+int r;
+printf("Enter the radius of the circle:");
+ scanf("%d", &r);
+ printf("the area of circle with radius %d is %f", r, 3.14*r*r);
 return 0;
 }
 

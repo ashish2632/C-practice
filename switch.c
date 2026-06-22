c50594848a85fe3a362a59a54e835bdc692dcc90
Ashish2632 <ashish2632a@gmail.com>
2026-06-22 15:34:02 +0000

    switch without case breking

---
+++ switch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main() {
+ int a;
+ printf("Enter a: ");
+ scanf("%d", &a);
+switch(a){
+case 1:
+printf("you entered 1\n");
+case 2:
+printf("you entered 2\n");
+}
+return 0;
+}
+

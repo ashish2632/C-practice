8d5e5c1d5f50d87e0ec97cfbd220d9ab371bf411
Ashish2632 <ashish2632a@gmail.com>
2026-06-24 15:42:32 +0000

    income and tax

---
+++ income.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+ float income, tax=0;
+printf("Enter your income:");
+ scanf("%f", &income);
+if(income<=250000){
+tax = 0;
+}
+else if(income>250000 && income<=500000){
+tax = 0.05 * (income-250000);
+}
+else if(income>500000 && income<=1000000){
+tax = 0.05 * (income-250000) + 0.2 * (income-500000);
+}
+else{
+tax = 0.05 * (income-250000) + 0.2 * (income-500000) + 0.3 * (income-1000000);
+}
+printf("Total tax is %f\n", tax);
+return 0;
+ }
+
